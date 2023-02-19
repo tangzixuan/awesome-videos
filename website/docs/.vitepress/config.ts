64d85fd69fe380b0b4414f52b6bb0aff0489314a
Zixuan Tang <3435677954@qq.com>
2023-02-19 23:47:25 +0800

    feat: change some description

--- website/docs/.vitepress/config.ts
+++ website/docs/.vitepress/config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    title: 'enjoy those Awesome-animate',
+    title: 'enjoy those Awesome-animate 🥳',
     description: 'enjoy it!',
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/tangzixuan/awesome-animate' },
+        ],
+    },
 })
